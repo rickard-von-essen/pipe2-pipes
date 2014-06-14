547aebf7fea86dcfcc648da26ce74faeefad164e
Rickard von Essen <rickard.von.essen@gmail.com>
2014-06-14 11:42:53 +0200

    parameterized pipeline

---
+++ jobs/pipeline.groovy
@@ -0,0 +1,31 @@
+env = System.getenv()
+
+project = env['project']
+git_url = env['git_url']
+branch = env['branch']
+
+job {
+    name "build-${project}"
+    scm {
+        git("${git_url}", "${branch}")
+    }
+    steps {
+        maven("clean install -B -DskipTests=true -Dproject.name=${project}")
+    }
+    publishers {
+      downstream("test-testclinic", "SUCCESS")
+    }
+}
+
+job {
+    name "test-${project}"
+    scm {
+        git("${git_url}", "${branch}")
+    }
+    steps {
+        maven("clean test -B -Dproject.name=${project}")
+    }
+    publishers {
+      archiveJunit("target/surefire-reports/*.xml", true, false, true)
+    }
+}
